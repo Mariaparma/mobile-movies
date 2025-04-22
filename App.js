2ca8b61b2b4f6239299cdc90188dff4f76195c39
maria parma <158210075+Mariaparma@users.noreply.github.com>
2025-04-22 08:22:27 -0300

    Update App.js

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from './pages/HomeScreen.js'; 
-import AvaliacaoScreen from './pages/AvaliacaoScreen.js'; // Corrigido o nome do componente
+import AvaliacaoScreen from './pages/AvaliacaoScreen.js'; 
 
 const Stack = createNativeStackNavigator();
 
